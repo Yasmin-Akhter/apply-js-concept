bf01c71496041dff23f3edf86e458a4183deb666
Yasmin-Akhter <yasminbd89@yahoo.com>
2022-01-31 11:55:18 +0600

    2nd commit

--- leap-year.js
+++ leap-year.js
@@ -11,3 +11,19 @@ function isLeapYear(year) {
 let myYear = 2000;
 const ismyYear = isLeapYear(myYear)
 console.log("Is my year", myYear, "is a leap year?", ismyYear);
+
+
+// checking even or odd number
+
+function chkNumber(number) {
+    if (number % 2 == 0) {
+        return "even";
+    }
+    else {
+        return "odd";
+    }
+}
+
+let myNumber = 34563;
+const result = chkNumber(myNumber);
+console.log("is", myNumber, "even or odd?", result);
