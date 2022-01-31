e3a71c7d31776b76bbc8d779a80861cb5e1af051
Yasmin-Akhter <yasminbd89@yahoo.com>
2022-01-31 09:22:51 +0600

    first commit

---
+++ leap-year.js
@@ -0,0 +1,13 @@
+// checking leap year
+
+function isLeapYear(year) {
+    if ((year % 4 == 0) && (year % 100 !== 0) || (year % 400 == 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+let myYear = 2000;
+const ismyYear = isLeapYear(myYear)
+console.log("Is my year", myYear, "is a leap year?", ismyYear);
